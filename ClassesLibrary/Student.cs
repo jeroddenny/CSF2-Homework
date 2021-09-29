b211c38c6504110e4ebd3cdb330852538ecd2978
Jerod Denny <jeroddenny@hotmail.com>
2021-09-29 18:38:50 -0500

    Student Complete

---
+++ ClassesLibrary/Student.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesLibrary
+{
+    public class Student
+    {
+        //fields
+        private string _firstName;
+        private string _lastName;
+        private string _studentID;
+        private float _gpa;
+
+
+        //properties
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+                                  
+        }
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+
+        }
+        public string StudentID
+        {
+            get { return _studentID; }
+            set { _studentID = value; }
+
+        }
+        public float GPA
+        {
+            get { return _gpa; }
+            set { _gpa = value; }
+
+        }
+
+        //Constructors (ctors)
+        public Student() { }
+        public Student(string firstName, string lastName, string id, float gpa)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            StudentID = _studentID;
+            GPA = gpa;
+        }
+
+       
+        //Methods 
+        public override string ToString()
+        {
+            return string.Format("Student: \nFirst Name: {0}\nLast Name: {1} \nID: {2} \nGPA {3}", FirstName, LastName, StudentID, GPA);
+        }
+        
+    }//end class
+}//end namespace
--- TesterProgram/Program.cs
+++ TesterProgram/Program.cs
@@ -3,13 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using ClassesLibrary;
 namespace TesterProgram
 {
     class Program
     {
         static void Main(string[] args)
         {
-        }
-    }
-}
+            Student s1 = new Student();
+
+                s1.FirstName = "Samus";
+                s1.LastName = "Aran";
+                s1.StudentID = "SRan";
+                s1.GPA = 3.5f;
+            
+
+            Console.WriteLine(s1);
+
+        }//end Main()
+    }//end class
+}//end namespace
